c0ab7efdca736161fea7ab72069169319a439dbb
Roman Dvornov <rdvornov@gmail.com>
2016-09-04 02:46:42 +0300

    initial implementation of CSS syntax grammar parser/stringifier

---
+++ test/grammar.js
@@ -0,0 +1,46 @@
+var assert = require('assert');
+var parse = require('../lib/grammar/parse.js');
+var stringify = require('../lib/grammar/stringify.js');
+var data = require('../data/data.json');
+
+function normalize(str) {
+    // Looks like there is no common rules for spaces (some syntaxes
+    // may have a extra space or miss some)
+    // e.g. rgba( <rgb-component>#{3} , <alpha-value> )
+    // but  hsl( <hue>, <percentage>, <percentage> )
+
+    return str.replace(/\B\s\B/g, '');
+}
+
+function createTest(name, syntax) {
+    return it(name, function() {
+        var res = parse(syntax);
+
+        assert.equal(res.type, 'Sequence');
+        assert.equal(normalize(stringify(res)), normalize(syntax));
+    });
+}
+
+describe('parse CSS syntax grammar', function() {
+    ['properties', 'syntaxes'].forEach(function(section) {
+        for (var name in data[section]) {
+            // currently broken
+            if (section === 'properties' && name === '-webkit-tap-highlight-color') {
+                continue;
+            }
+
+            // odd syntax
+            if (section === 'syntaxes' && name === 'an-plus-b') {
+                continue;
+            }
+
+            var info = data[section][name];
+            var syntax = (info.syntax || info)
+                .replace(/&lt;/g, '<')
+                .replace(/&gt;/g, '>')
+                .replace(/&amp;/g, '&');
+
+            createTest(section + '/' + name, syntax);
+        }
+    });
+});
