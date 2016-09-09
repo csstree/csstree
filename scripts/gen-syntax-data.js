4f3434b0048069be65b7994ecef8b6590945b8af
Roman Dvornov <rdvornov@gmail.com>
2016-09-09 14:21:25 +0300

    implement module with default syntax

--- scripts/gen-syntax-data.js
+++ scripts/gen-syntax-data.js
@@ -1,16 +1,8 @@
 var resultFilename = require('path').resolve(__dirname + '/../docs/syntax.json');
-var data = require('../data');
-var syntax = require('../lib/syntax').create({
-    generic: true
-});
-
-for (var key in data.properties) {
-    syntax.addProperty(key, data.properties[key].syntax);
-}
-
-for (var key in data.syntaxes) {
-    syntax.addType(key, data.syntaxes[key]);
-}
+var syntax = require('../lib/default');
 
 console.log('Write data to ' + resultFilename);
-require('fs').writeFileSync(resultFilename, syntax);
+require('fs').writeFileSync(
+  resultFilename,
+  syntax
+);
