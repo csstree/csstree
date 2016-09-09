a0968d0bbbc38c3f63c8d3619a49044583ed12ae
Roman Dvornov <rdvornov@gmail.com>
2016-09-09 14:44:03 +0300

    fix path to default syntax

--- scripts/gen-syntax-data.js
+++ scripts/gen-syntax-data.js
@@ -1,5 +1,5 @@
 var resultFilename = require('path').resolve(__dirname + '/../docs/syntax.json');
-var syntax = require('../lib/default');
+var syntax = require('../lib/syntax/default');
 
 console.log('Write data to ' + resultFilename);
 require('fs').writeFileSync(
