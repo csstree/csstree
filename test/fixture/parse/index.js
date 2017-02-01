2b2d88bfd0b848cdfcd11db88bff5c4a43f4f959
Roman Dvornov <rdvornov@gmail.com>
2017-02-01 14:40:56 +0300

    store known functional-pseudo and function tests aside

--- test/fixture/parse/index.js
+++ test/fixture/parse/index.js
@@ -44,43 +44,50 @@ function forEachTest(factory, errors) {
 }
 
 var tests = fs.readdirSync(__dirname).reduce(function(result, scope) {
-    var dir = path.join(__dirname, scope);
+    function scanDir(dir) {
+        if (fs.statSync(dir).isDirectory()) {
+            fs.readdirSync(dir).forEach(function(fn) {
+                var filename = path.join(dir, fn);
 
-    if (fs.statSync(dir).isDirectory()) {
-        fs.readdirSync(dir).forEach(function(fn) {
-            var filename = path.join(dir, fn);
-            var tests = require(filename);
-            var errors = {};
-            var locator = new JsonLocator(filename);
-
-            for (var key in tests) {
-                tests[key].name = locator.get(key);
-                if (tests[key].error) {
-                    if (typeof tests[key].offset === 'string') {
-                        var offset = tests[key].offset.indexOf('^');
-                        var lines = tests[key].source.substr(0, offset).split(/\r|\r\n|\n|\f/g);
-                        var position = {
-                            offset: offset,
-                            line: lines.length,
-                            column: lines.pop().length + 1
-                        };
-
-                        tests[key].position = position;
-                    }
-                    errors[key] = tests[key];
-                    delete tests[key];
-                } else if (tests[key].ast.type.toLowerCase() !== scope.toLowerCase() && wrapper.hasOwnProperty(scope)) {
-                    tests[key].ast = wrapper[scope](tests[key].ast);
+                if (fs.statSync(filename).isDirectory()) {
+                    return scanDir(filename);
                 }
-            }
 
-            result[filename] = {
-                scope: scope,
-                tests: tests,
-                errors: errors
-            };
-        });
+                var tests = require(filename);
+                var errors = {};
+                var locator = new JsonLocator(filename);
+
+                for (var key in tests) {
+                    tests[key].name = locator.get(key);
+                    if (tests[key].error) {
+                        if (typeof tests[key].offset === 'string') {
+                            var offset = tests[key].offset.indexOf('^');
+                            var lines = tests[key].source.substr(0, offset).split(/\r|\r\n|\n|\f/g);
+                            var position = {
+                                offset: offset,
+                                line: lines.length,
+                                column: lines.pop().length + 1
+                            };
+
+                            tests[key].position = position;
+                        }
+                        errors[key] = tests[key];
+                        delete tests[key];
+                    } else if (tests[key].ast.type.toLowerCase() !== scope.toLowerCase() && wrapper.hasOwnProperty(scope)) {
+                        tests[key].ast = wrapper[scope](tests[key].ast);
+                    }
+                }
+
+                result[filename] = {
+                    scope: scope,
+                    tests: tests,
+                    errors: errors
+                };
+            });
+        }
     }
+
+    scanDir(path.join(__dirname, scope));
 
     return result;
 }, {});
