d6c348ecbe598b70fab17351a28fd8c9c950b8b1
Roman Dvornov <rdvornov@gmail.com>
2017-03-01 21:16:48 +0300

    fix syntax json generation

--- scripts/gen-syntax-data.js
+++ scripts/gen-syntax-data.js
@@ -1,5 +1,5 @@
 var resultFilename = require('path').resolve(__dirname + '/../dist/default-syntax.json');
-var defaultSyntax = require('../lib').defaultLexer;
+var defaultSyntax = require('../lib').lexer;
 
 console.log('Write data to ' + resultFilename);
 require('fs').writeFileSync(
