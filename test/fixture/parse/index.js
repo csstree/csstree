320df92df1e65602d0fd88e1fb081ce4e3c99184
Roman Dvornov <rdvornov@gmail.com>
2017-03-08 23:18:41 +0300

    remove redundant translate field in tests

--- test/fixture/parse/index.js
+++ test/fixture/parse/index.js
@@ -63,6 +63,10 @@ var tests = fs.readdirSync(__dirname).reduce(function(result, scope) {
                         }
                         errors[storeKey] = test;
                     } else {
+                        if (test.source === test.translate) {
+                            console.warn('[WARN] Test `source` and `translate` fields are equal: ' + test.name);
+                        }
+
                         if (test.ast.type.toLowerCase() !== scope.toLowerCase() && wrapper.hasOwnProperty(scope)) {
                             test.ast = wrapper[scope](test.ast);
                         }
