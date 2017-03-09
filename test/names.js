9e5161f74f72dfcac2a06d8fcb5279f9ea56413c
Roman Dvornov <rdvornov@gmail.com>
2017-03-09 04:08:11 +0300

    additional declaration hacks (# and +)

--- test/names.js
+++ test/names.js
@@ -144,7 +144,7 @@ describe('names utils', function() {
         });
 
         describe('should detect hacks', function() {
-            ['*', '_', '$', '//'].forEach(function(hack) {
+            ['*', '_', '$', '#', '+', '//'].forEach(function(hack) {
                 it(hack, function() {
                     assert.deepEqual(property(hack + 'test'), {
                         name: 'test',
