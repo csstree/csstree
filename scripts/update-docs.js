77c00074d6b4fd0c4fd2d50bc9686ce6cda2bd6d
Roman Dvornov <rdvornov@gmail.com>
2017-08-29 22:05:20 +0300

    add lint to check docs are up to date

--- scripts/update-docs.js
+++ scripts/update-docs.js
@@ -1,19 +1,30 @@
 var path = require('path');
 var fs = require('fs');
+var checkUpdatesNeeded = process.argv[2] === '--lint';
 
 fs.readdirSync(path.resolve(__dirname, 'docs')).forEach(function(filename) {
     var name = path.basename(filename);
     var docsFilename = path.resolve(__dirname, '../docs', name.replace(/\.js$/, '.md'));
     var updateFn = require('./docs/' + filename);
 
-    console.log('Update ' + docsFilename);
+    if (!checkUpdatesNeeded) {
+        console.log('Synchronizing ' + docsFilename);
+    }
+
     var currentContent = fs.readFileSync(docsFilename, 'utf8').replace(/\r\n/g, '\n');
     var newContent = updateFn(currentContent);
 
     if (typeof newContent === 'string' && newContent !== currentContent) {
+        if (checkUpdatesNeeded) {
+            console.error('File ' + docsFilename + ' requires to be up to date.\nRun `npm run update:docs` to fix the problem');
+            process.exit(2);
+        }
+
         fs.writeFileSync(docsFilename, newContent, 'utf8');
         console.log('  Updated');
     } else {
-        console.log('  No update');
+        if (!checkUpdatesNeeded) {
+            console.log('  No updates');
+        }
     }
 });
