fa004f86fe00a60ab0a3397ece6218aafbfe06c5
Roman Dvornov <rdvornov@gmail.com>
2017-10-12 02:38:58 +0300

    add known dicts as Lexer#defs

--- data/index.js
+++ data/index.js
@@ -3,7 +3,16 @@ var mdnSyntaxes = require('mdn-data/css/syntaxes.json');
 var patch = require('./patch.json');
 var data = {
     properties: {},
-    types: {}
+    types: {},
+    defs: {
+        Atrule: require('./known/Atrule.json').items,
+        Declaration: require('./known/Declaration.json').items,
+        Dimension: require('./known/Dimension.json').items,
+        Function: require('./known/Function.json').items,
+        MediaFeature: require('./known/MediaFeature.json').items,
+        PseudoClassSelector: require('./known/PseudoClassSelector.json').items,
+        PseudoElementSelector: require('./known/PseudoElementSelector.json').items
+    }
 };
 
 function normalizeSyntax(syntax) {
@@ -42,5 +51,29 @@ for (var key in mdnProperties) {
 for (var key in mdnSyntaxes) {
     data.types[key] = normalizeSyntax(mdnSyntaxes[key].syntax);
 }
+
+// normalize defs
+Object.keys(data.defs).forEach(function(type) {
+    var items = data.defs[type];
+
+    if (Array.isArray(items)) {
+        data.defs[type] = items.reduce(function(res, item) {
+            var value = true;
+
+            if (type === 'PseudoClassSelector' || type === 'PseudoElementSelector') {
+                if (/\(\)$/.test(item)) {
+                    item = item.substr(0, item.length - 2);
+                    value = 2; // functional pseudo
+                } else {
+                    value = 1; // non-function pseudo
+                }
+                value |= res[item] || 0;  // 3 – both
+            }
+
+            res[item] = value;
+            return res;
+        }, Object.create(null));
+    }
+});
 
 module.exports = data;
