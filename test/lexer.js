20b9ce8bc289a4a30e916e95bf90a50121607ad8
Roman Dvornov <rdvornov@gmail.com>
2017-11-03 03:36:33 +0300

    added Lexer#match
    - implemented Lexer#match(syntax, node)
    - relaxed test for node in any match* method; a node can be any type now, but should has a children

--- test/lexer.js
+++ test/lexer.js
@@ -395,6 +395,51 @@ describe('lexer', function() {
         });
     });
 
+    describe('match()', function() {
+        var value = parseCss('fn(1, 2, 3)', { context: 'value' });
+        var fn = value.children.first();
+        var customSyntax = syntax.fork(function(prev, assign) {
+            return assign(prev, {
+                types: {
+                    foo: '<bar>#',
+                    bar: '[ 1 | 2 | 3 ]',
+                    fn: 'fn(<foo>)'
+                }
+            });
+        });
+
+        it('should match by type', function() {
+            var syntax = customSyntax.lexer.getType('foo');
+            var match = customSyntax.lexer.match(syntax, fn);
+
+            assert(match.matched);
+            assert.equal(match.error, null);
+        });
+
+        it('should match by arbitrary node of syntax (function)', function() {
+            var syntax = customSyntax.lexer.getType('fn').syntax.terms[0];
+            var match = customSyntax.lexer.match(syntax, value);
+
+            assert(match.matched);
+            assert.equal(match.error, null);
+        });
+
+        it('should match by arbitrary node of syntax (parameters of function)', function() {
+            var syntax = customSyntax.lexer.getType('fn').syntax.terms[0].children;
+            var match = customSyntax.lexer.match(syntax, fn);
+
+            assert(match.matched);
+            assert.equal(match.error, null);
+        });
+
+        it('should fails on bad syntax', function() {
+            var match = customSyntax.lexer.match({}, fn);
+
+            assert.equal(match.matched, null);
+            assert.equal(match.error.message, 'Bad syntax');
+        });
+    });
+
     describe('mismatch node', function() {
         var customSyntax = syntax.fork(function(prev, assign) {
             return assign(prev, {
