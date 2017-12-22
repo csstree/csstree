5f23a2e3e67e48ec2ce8c784ff5284560353c717
Roman Dvornov <rdvornov@gmail.com>
2017-12-22 04:19:22 +0300

    change generator API and add support for AST with arrays
    - generator is supported an AST which uses Arrays for children
    - processChunk became a method of context (this)
    - rename context methods
      * generate -> node
      * each -> children
      * processChunk -> chunk

--- test/translate.js
+++ test/translate.js
@@ -1,22 +1,27 @@
 var assert = require('assert');
 var parse = require('../lib').parse;
 var translate = require('../lib').translate;
+var toPlainObject = require('../lib').toPlainObject;
 var forEachParseTest = require('./fixture/parse').forEachTest;
 var stringify = require('./helpers/stringify');
 
-function createTranslateTest(name, test) {
-    (test.skip ? it.skip : it)(name, function() {
-        var ast = parse(test.source, test.options);
-        var restoredCss = translate(ast);
+function createTranslateTests(name, test) {
+    var ast = parse(test.source, test.options);
+    var restoredCss = translate(ast);
+    // FIXME: Skip some test cases for round-trip check until generator's improvements
+    var skipRoundTrip = test.skip || /block at-rule #c\.2|atruler\.c\.2|parentheses\.c\.3/.test(name);
 
+    (test.skip ? it.skip : it)(name, function() {
         // strings should be equal
         assert.equal(restoredCss, 'translate' in test ? test.translate : test.source);
+    });
 
-        // FIXME: Skip some test cases for round-trip check until generator's improvements
-        if (/block at-rule #c\.2|atruler\.c\.2|parentheses\.c\.3/.test(name)) {
-            return;
-        }
+    (test.skip ? it.skip : it)(name + ' (plain object)', function() {
+        // strings should be equal
+        assert.equal(translate(toPlainObject(ast)), 'translate' in test ? test.translate : test.source);
+    });
 
+    (skipRoundTrip ? it.skip : it)(name + ' (round-trip)', function() {
         // https://drafts.csswg.org/css-syntax/#serialization
         // The only requirement for serialization is that it must "round-trip" with parsing,
         // that is, parsing the stylesheet must produce the same data structures as parsing,
@@ -30,7 +35,7 @@ function createTranslateTest(name, test) {
 }
 
 describe('translate', function() {
-    forEachParseTest(createTranslateTest);
+    forEachParseTest(createTranslateTests);
 
     it('should throws on unknown node type', function() {
         assert.throws(function() {
