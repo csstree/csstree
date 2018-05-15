197cf86423c8a9d008ee7fd08bb0631309bec0f7
Roman Dvornov <rdvornov@gmail.com>
2018-05-15 03:06:56 +0300

    use Enum for Function and functional Type

--- scripts/test-grammar-parse-generate.js
+++ scripts/test-grammar-parse-generate.js
@@ -20,6 +20,45 @@ iterateSyntaxes(function(section, name, syntax) {
         const restored = generate(ast);
         const roundtrip = generate(parse(restored));
 
+        if (name.slice(-2) === '()') {
+            if (ast.type === 'Group') {
+                const functionName = name.slice(0, -2);
+                const isCorrectTerm = term => term.type === 'Function' && term.name === functionName;
+
+                switch (ast.combinator) {
+                    case ' ':
+                        if (!isCorrectTerm(ast.terms[0])) {
+                            reason = 'the first token of top level Group is not a function with expected name';
+                            throw new Error('Bad syntax');
+                        }
+                        break;
+
+                    case '|':
+                        const allTermsIsCorrect = ast.terms.every(term => {
+                            return (
+                                isCorrectTerm(term)
+                                ||
+                                (term.type === 'Group' && isCorrectTerm(term.terms[0]))
+                            );
+                        });
+
+                        if (!allTermsIsCorrect) {
+                            reason = 'some of top level Group is not a function with expected name';
+                            throw new Error('Bad syntax');
+                        }
+
+                        break;
+
+                    default:
+                        reason = 'a root node should be a Group with combinator " " or "|"';
+                        throw new Error('Bad syntax');
+                }
+            } else {
+                reason = 'a root node should be a Group';
+                throw new Error('Bad AST');
+            }
+        }
+
         if (roundtrip !== restored) {
             reason = 'roundtrip broken';
             throw new Error([
