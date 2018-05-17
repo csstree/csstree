c58f26b805b39cb75af792ec2d822daa3d65fbc3
Roman Dvornov <rdvornov@gmail.com>
2018-05-17 21:56:17 +0300

    script for collecting a data

---
+++ scripts/collect-data.js
@@ -0,0 +1,63 @@
+const iterateSyntaxes = require('./utils/iterate-syntaxes');
+const parse = require('../lib').grammar.parse;
+const walk = require('../lib').grammar.walk;
+const generate = require('../lib').grammar.generate;
+const collected = {
+    Function: new Set(),
+    String: new Set()
+};
+
+iterateSyntaxes(function(section, name, syntax) {
+    const id = `${section} "${name}"`;
+
+    try {
+        const ast = parse(syntax);
+
+        walk(ast, function(node) {
+            switch (node.type) {
+                case 'Function':
+                    collected.Function.add(node.name + '()');
+                    break;
+                case 'String':
+                    collected.String.add(node.value);
+                    break;
+            }
+        });
+
+        // const stack = [];
+        // walk(ast, {
+        //     enter: function(node) {
+        //         if (node.type === 'Function') {
+        //             const parent = stack[stack.length - 1];
+        //             const start = parent.terms.indexOf(node);
+
+        //             for (var i = start; i < parent.terms.length; i++) {
+        //                 if (parent.terms[i].type === 'Token' && parent.terms[i].value === ')') {
+        //                     i++;
+        //                     break;
+        //                 }
+        //             }
+
+        //             const functionSequence = {
+        //                 type: 'Group',
+        //                 terms: parent.terms.slice(start, i),
+        //                 combinator: ' ',
+        //                 explicit: false,
+        //                 disallowEmpty: false
+        //             };
+        //         }
+        //         stack.push(node);
+        //     },
+        //     leave: function() {
+        //         stack.pop();
+        //     }
+        // });
+    } catch (e) {
+        console.error(id, e);
+    }
+});
+
+for (const type in collected) {
+    console.log(type);
+    console.log(JSON.stringify([...collected[type]].sort(), null, 4));
+}
