95e97582fb932a051bcc78418ab057d4fc9f77a5
Roman Dvornov <rdvornov@gmail.com>
2020-01-13 22:54:39 +0300

    Rework syntax dump & build, drop dist/default-syntax.json

--- scripts/gen-syntax-data.js
+++
@@ -1,8 +0,0 @@
-var resultFilename = require('path').resolve(__dirname + '/../dist/default-syntax.json');
-var defaultSyntax = require('../lib').lexer;
-
-console.log('Write data to ' + resultFilename);
-require('fs').writeFileSync(
-    resultFilename,
-    defaultSyntax
-);
