a8accabb28f5ae9efd39aaed3e6f016b0be95c7c
Roman Dvornov <rdvornov@gmail.com>
2020-01-16 23:29:39 +0500

    Add roundtrip script

---
+++ scripts/roundtrip.js
@@ -0,0 +1,16 @@
+const cli = require('clap');
+const { parse, generate } = require('../lib');
+
+cli.command('parse', '[css]')
+    .option('-c, --context <context>')
+    .option('--no-parse-atrule-prelude')
+    .option('--no-parse-rule-prelude')
+    .option('--no-parse-value')
+    .option('--parse-custom-property')
+    .action(function(args) {
+        const source = args[0] || '';
+        const unescaped = JSON.parse(`"${source.replace(/\\.|"/g, m => m === '"' ? '\\"' : m)}"`);
+
+        console.log(generate(parse(unescaped, this.values)));
+    })
+    .run();
