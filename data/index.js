9d32b3a6171a73521234316d94e575d8761f2ce2
Roman Dvornov <rdvornov@gmail.com>
2020-11-16 00:21:47 +0100

    Add an option to append syntax not override

--- data/index.js
+++ data/index.js
@@ -1,19 +1,20 @@
-var mdnAtrules = require('mdn-data/css/at-rules.json');
-var mdnProperties = require('mdn-data/css/properties.json');
-var mdnSyntaxes = require('mdn-data/css/syntaxes.json');
-var patch = require('./patch.json');
+const mdnAtrules = require('mdn-data/css/at-rules.json');
+const mdnProperties = require('mdn-data/css/properties.json');
+const mdnSyntaxes = require('mdn-data/css/syntaxes.json');
+const patch = require('./patch.json');
+const extendSyntax = /^\s*\|\s*/;
 
 function preprocessAtrules(dict) {
-    var result = Object.create(null);
+    const result = Object.create(null);
 
-    for (var atruleName in dict) {
-        var atrule = dict[atruleName];
-        var descriptors = null;
+    for (const atruleName in dict) {
+        const atrule = dict[atruleName];
+        let descriptors = null;
 
         if (atrule.descriptors) {
             descriptors = Object.create(null);
 
-            for (var descriptor in atrule.descriptors) {
+            for (const descriptor in atrule.descriptors) {
                 descriptors[descriptor] = atrule.descriptors[descriptor].syntax;
             }
         }
@@ -31,21 +32,23 @@ function buildDictionary(dict, patchDict) {
     var result = {};
 
     // copy all syntaxes for an original dict
-    for (var key in dict) {
+    for (const key in dict) {
         result[key] = dict[key].syntax;
     }
 
     // apply a patch
-    for (var key in patchDict) {
+    for (const key in patchDict) {
         if (key in dict) {
             if (patchDict[key].syntax) {
-                result[key] = patchDict[key].syntax;
+                result[key] = extendSyntax.test(patchDict[key].syntax)
+                    ? result[key] + ' ' + patchDict[key].syntax.trim()
+                    : patchDict[key].syntax;
             } else {
                 delete result[key];
             }
         } else {
             if (patchDict[key].syntax) {
-                result[key] = patchDict[key].syntax;
+                result[key] = patchDict[key].syntax.replace(extendSyntax, '');
             }
         }
     }
