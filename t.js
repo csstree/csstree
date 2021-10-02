37e9832d6427522e2e14e5482ddfd6cb0cafaca2
Roman Dvornov <rdvornov@gmail.com>
2021-10-02 14:41:24 +0200

    Delete wrongly added file

--- t.js
+++
@@ -1,6 +0,0 @@
-import * as x from './lib/index.js';
-// import * as x2 from './lib/tokenizer/index.js';
-// import x3 from './package.json';
-
-// console.log(csstree.generate(csstree.parse('.a { color: red }')));
-console.log(x.parse(':matches(.a{)', { context: 'selector' }));
