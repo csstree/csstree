4332d41862d28a956369c883aeb3af1a96f4a95b
Roman Dvornov <rdvornov@gmail.com>
2021-10-03 17:29:00 +0200

    Change bundle set to provide `dist/csstree.js` (iife) and `dist/csstree.esm.js`

--- scripts/bundle.js
+++ scripts/bundle.js
@@ -1,14 +1,13 @@
 import path from 'path';
 import esbuild from 'esbuild';
-import csstree from '../lib/index.js';
+import { lexer } from '../lib/index.js';
 import { createRequire } from 'module';
 
-const { lexer } = csstree;
 const { version } = createRequire(import.meta.url)('../package.json');
 
 async function build() {
     const genModules = {
-        [path.resolve('data/index.js')]: () => `export default ${JSON.stringify(lexer.dump(), null, 4)};`,
+        [path.resolve('lib/data.js')]: () => `export default ${JSON.stringify(lexer.dump(), null, 4)};`,
         [path.resolve('lib/version.js')]: () => `export const version = "${version}";`
     };
     const genModulesFilter = new RegExp('(' + Object.keys(genModules).join('|').replace(/\./g, '\\.') + ')$');
@@ -32,19 +31,21 @@ async function build() {
     esbuild.build({
         entryPoints: ['lib/index.js'],
         outfile: 'dist/csstree.js',
-        format: 'esm',
+        format: 'iife',
+        globalName: 'csstree',
         bundle: true,
+        minify: true,
         logLevel: 'info',
         plugins
     });
 
     esbuild.build({
         entryPoints: ['lib/index.js'],
-        outfile: 'dist/csstree.min.js',
+        outfile: 'dist/csstree.esm.js',
         format: 'esm',
         bundle: true,
-        logLevel: 'info',
         minify: true,
+        logLevel: 'info',
         plugins
     });
 }
