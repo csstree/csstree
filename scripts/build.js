6a5115930baf84b6114835fdae4d9e4728d94c9e
gabriel miranda <gabrielmfern@outlook.com>
2025-09-12 14:57:32 -0300

    single build script to generate cjs version

---
+++ scripts/build.js
@@ -0,0 +1,168 @@
+import esbuild from 'esbuild';
+import url from 'url';
+import fs from 'fs';
+import packageJson from '../package.json' with { type: 'json' };
+import path from 'path';
+import { lexer } from '../src/index.js';
+import { rollup, watch } from 'rollup';
+
+const { version } = packageJson;
+
+const data = JSON.stringify(lexer.dump(), null, 4);
+
+async function bundle() {
+    const genModules = {
+        'data.js': `export default ${data};`,
+        'data.cjs': `module.exports = ${data};`,
+        'version.js': `export const version = "${version}";`,
+        'version.cjs': `module.exports = "${version}";`
+    };
+    const genModulesFilter = new RegExp('src[\\\\/](' + Object.keys(genModules).join('|').replace(/\./g, '\\.') + ')$');
+    const plugins = [{
+        name: 'replace',
+        setup({ onLoad }) {
+            onLoad({ filter: genModulesFilter }, args => ({
+                contents: genModules[path.basename(args.path)]
+            }));
+        }
+    }];
+
+    await Promise.all([
+        esbuild.build({
+            entryPoints: ['src/index.js'],
+            outfile: 'dist/bundled/index.life.js',
+            format: 'iife',
+            globalName: 'csstree',
+            bundle: true,
+            minify: true,
+            logLevel: 'info',
+            plugins
+        }),
+
+        esbuild.build({
+            entryPoints: ['src/index.js'],
+            outfile: 'dist/bundled/index.js',
+            format: 'esm',
+            bundle: true,
+            minify: true,
+            logLevel: 'info',
+            plugins
+        })
+    ]);
+
+    for (const [key, value] of Object.entries(genModules)) {
+        const fn = path.basename(key);
+
+        fs.writeFileSync(`dist/bundled/${fn}`, value);
+    }
+}
+
+bundle();
+
+async function buildCJS() {
+    const treeshake = 'smallest'; // see https://rollupjs.org/guide/en/#treeshake
+    const patchImportSelf = 'auto'; // 'auto' | false | true
+    const testFilePattern = /\/__tests\//;
+    const external = [
+        'fs',
+        'path',
+        'assert',
+        'json-to-ast',
+        'css-tree',
+        'css-tree/tokenizer',
+        'css-tree/parser',
+        'css-tree/selector-parser',
+        'css-tree/generator',
+        'css-tree/walker',
+        'css-tree/definition-syntax',
+        'css-tree/definition-syntax-data',
+        'css-tree/definition-syntax-data-patch',
+        'css-tree/lexer',
+        'css-tree/convertor',
+        'css-tree/utils',
+        /^source-map/
+    ];
+    const entryPoints = [
+        './src/index.js',
+        './src/tokenizer/index.js',
+        './src/parser/index.js',
+        './src/parser/parse-selector.js',
+        './src/generator/index.js',
+        './src/walker/index.js',
+        './src/lexer/index.js',
+        './src/convertor/index.js',
+        './src/utils/index.js',
+        ...readDir('./src/__tests')
+    ];
+    const outputDir = './dist/cjs';
+
+    const startTime = Date.now();
+
+    console.log();
+    console.log(`Convert ESM to CommonJS (output: ${outputDir})`);
+
+    const inputOptions = {
+        input: entryPoints,
+        external,
+        treeshake,
+        plugins: [
+            hoistJsonImports(),
+        ]
+    };
+    const outputOptions = {
+        dir: outputDir,
+        entryFileNames: '[name].cjs',
+        format: 'cjs',
+        exports: 'auto',
+        preserveModules: true,
+        interop: false,
+        esModule: false,
+        generatedCode: {
+            constBindings: true
+        }
+    };
+
+    console.log();
+    console.log(`Convert ESM to CommonJS (output: ${outputDir})`);
+
+    const bundle = await rollup(inputOptions);
+    await bundle.write(outputOptions);
+    await bundle.close();
+
+    console.log(`Done in ${Date.now() - startTime}ms`);
+
+
+    //
+    // helpers
+    //
+
+    function readDir(dir, pattern = /\.js$/) {
+        return fs.readdirSync(dir)
+            .map(fn => `${dir}/${fn}`)
+            .filter(fn => fs.statSync(fn).isFile() && pattern.test(fn));
+    }
+
+    // The `import ... from ... with type { 'json' }` syntax is not supported by the Node.js versions we still support (<15.0.0)
+    // so we simply hoist the JSON into the JavaScript code that imported them with the syntax
+    function hoistJsonImports() {
+        return {
+            name: 'hoist-json-imports',
+            /**
+              * @param {string} code
+              */
+            transform(code, filename) {
+                return code
+                    .replaceAll(
+                        /import\s+(\S+)\s+from\s+['"]([^']+)['"]\s+with\s+{\s+type:\s+['"]json['"]\s+}/gm,
+                        (_fullImport, specifier, importPath) => {
+                            const jsonPath = url.fileURLToPath(import.meta.resolve(importPath, filename));
+                            const json = fs.readFileSync(jsonPath, 'utf8');
+                            return `const ${specifier} = ${json.trim()};`;
+                        }
+                    );
+            }
+        };
+    }
+}
+
+buildCJS();
