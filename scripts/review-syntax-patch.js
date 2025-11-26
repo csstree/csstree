c5866547defec7a0faa0c9e020a832cc9aaafa87
Claas Augner <caugner@mozilla.com>
2025-11-27 00:08:36 +0100

    chore: migrate from mdn-data to @webref/css

--- scripts/review-syntax-patch.js
+++ scripts/review-syntax-patch.js
@@ -1,7 +1,10 @@
 import fs from 'fs';
 import path from 'path';
-import data from 'mdn-data/css/index.js';
+import { createRequire } from 'module';
 import { fileURLToPath } from 'url';
+
+const require = createRequire(import.meta.url);
+const webrefCss = require('@webref/css/css.json');
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const patchFilename = path.join(__dirname, '../data/patch.json');
@@ -9,20 +12,57 @@ const currentContent = fs.readFileSync(patchFilename, 'utf8').replace(/\r\n/g, '
 const csstreePatch = JSON.parse(currentContent);
 const checkUpdatesNeeded = process.argv[2] === '--lint';
 
-function checkSection(section, mdnDataSection = section) {
+// Convert @webref/css arrays to indexed objects
+const data = {
+    properties: {},
+    syntaxes: {}
+};
+
+for (const property of webrefCss.properties) {
+    // Only include properties that have syntax
+    if (property.syntax) {
+        data.properties[property.name] = {
+            syntax: property.syntax
+        };
+    }
+}
+
+// Only include types that have syntax definitions
+// Types without syntax in @webref/css can be provided by patch.json
+for (const type of webrefCss.types) {
+    if (type.syntax) {
+        data.syntaxes[type.name] = {
+            syntax: type.syntax
+        };
+    }
+}
+
+// Also include functions from @webref/css
+// In mdn-data, functions were stored in syntaxes, but @webref/css separates them
+// Only include functions that have syntax definitions
+for (const func of webrefCss.functions) {
+    if (func.syntax) {
+        // Function names in @webref/css include parentheses (e.g., "abs()")
+        data.syntaxes[func.name] = {
+            syntax: func.syntax
+        };
+    }
+}
+
+function checkSection(section, webrefDataSection = section) {
     Object.keys(csstreePatch[section]).forEach(function(name) {
         const csstreeSyntax = csstreePatch[section][name];
-        const mdnData = data[mdnDataSection][name];
+        const webrefData = data[webrefDataSection][name];
         const id = `${section}/${name}`;
 
-        if (mdnData && csstreeSyntax.syntax === mdnData.syntax.replace(/[ ]*\n[ ]*/g, ' ')) {
+        if (webrefData && csstreeSyntax.syntax === webrefData.syntax.replace(/[ ]*\n[ ]*/g, ' ')) {
             delete csstreePatch[section][name];
 
             if (checkUpdatesNeeded) {
-                console.error(id, 'is equal to mdn/data syntax (patch is redundant)');
+                console.error(id, 'is equal to @webref/css syntax (patch is redundant)');
                 console.log('   ', [
                     csstreeSyntax.syntax,
-                    mdnData.syntax
+                    webrefData.syntax
                 ].join('\n    '));
                 console.log();
             } else {
--- scripts/utils/iterate-syntaxes.js
+++ scripts/utils/iterate-syntaxes.js
@@ -1,27 +1,89 @@
-import data from 'mdn-data/css/index.js';
+import { createRequire } from 'module';
 import csstreeData from '../../lib/data.js';
+
+const require = createRequire(import.meta.url);
+const webrefCss = require('@webref/css/css.json');
+
+// Convert @webref/css arrays to indexed objects for easier access
+const data = {
+    atRules: {},
+    properties: {},
+    syntaxes: {}
+};
+
+for (const atrule of webrefCss.atrules) {
+    // Only include at-rules that have syntax
+    if (atrule.syntax) {
+        data.atRules[atrule.name] = {
+            syntax: atrule.syntax
+        };
+
+        if (atrule.descriptors) {
+            data.atRules[atrule.name].descriptors = {};
+            for (const descriptor of atrule.descriptors) {
+                // Only include descriptors that have syntax
+                if (descriptor.syntax) {
+                    data.atRules[atrule.name].descriptors[descriptor.name] = {
+                        syntax: descriptor.syntax
+                    };
+                }
+            }
+        }
+    }
+}
+
+for (const property of webrefCss.properties) {
+    // Only include properties that have syntax
+    if (property.syntax) {
+        data.properties[property.name] = {
+            syntax: property.syntax
+        };
+    }
+}
+
+// Only include types that have syntax definitions
+// Types without syntax in @webref/css can be provided by patch.json
+for (const type of webrefCss.types) {
+    if (type.syntax) {
+        data.syntaxes[type.name] = {
+            syntax: type.syntax
+        };
+    }
+}
+
+// Also include functions from @webref/css
+// In mdn-data, functions were stored in syntaxes, but @webref/css separates them
+// Only include functions that have syntax definitions
+for (const func of webrefCss.functions) {
+    if (func.syntax) {
+        // Function names in @webref/css include parentheses (e.g., "abs()")
+        data.syntaxes[func.name] = {
+            syntax: func.syntax
+        };
+    }
+}
 
 export default function iterateSyntaxes(fn) {
     for (const name in data.atRules) {
-        fn('mdn/atRules', name, data.atRules[name].syntax);
+        fn('webref/atRules', name, data.atRules[name].syntax);
         for (const descriptor in data.atRules[name].descriptors) {
-            fn('mdn/atRules/' + name, descriptor, data.atRules[name].descriptors[descriptor].syntax);
+            fn('webref/atRules/' + name, descriptor, data.atRules[name].descriptors[descriptor].syntax);
         }
     }
 
     for (const name in data.properties) {
-        fn('mdn/properties', name, data.properties[name].syntax);
+        fn('webref/properties', name, data.properties[name].syntax);
     }
 
     for (const name in data.syntaxes) {
-        fn('mdn/syntaxes', name, data.syntaxes[name].syntax);
+        fn('webref/syntaxes', name, data.syntaxes[name].syntax);
     }
 
     ['properties', 'types'].forEach(function(section) {
         Object.keys(csstreeData[section]).forEach(function(name) {
             const csstreeSyntax = csstreeData[section][name];
-            const mdnData = data[section === 'properties' ? 'properties' : 'syntaxes'][name];
-            if (!mdnData || csstreeSyntax !== mdnData.syntax) {
+            const webrefData = data[section === 'properties' ? 'properties' : 'syntaxes'][name];
+            if (!webrefData || csstreeSyntax !== webrefData.syntax) {
                 fn('csstree/' + section, name, csstreeSyntax);
             }
         });
